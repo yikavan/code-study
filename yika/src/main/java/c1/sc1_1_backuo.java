8e81df00d9799ed28eda0ec846cfea874019b58d
Yika <freedarknessdeep@gmail.com>
2019-05-27 01:11:09 +0800

    simplyfied the codes

--- yika/src/main/java/c1/sc1_1_backuo.java
+++ yika/src/main/java/c1/sc1_1_backuo.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /**
  * sc1_1
  */
-public class sc1_1 {
+public class sc1_1_backuo {
     public static void main(String[] args) {
         float[] a = new float[50], b = new float[50];
         Scanner sc = new Scanner(System.in);
@@ -49,27 +49,21 @@ public class sc1_1 {
         a[0] = a[1];
         b[0] = b[1];
         // 开始判断
-        // 找到左半边 (m+n+1)/2 个数中最大的
-        // 可以作为左半边最大的数可以是a[p],b[q-1],a[p-1],b[q]
         while (flag == 1) {
             if (a[p] <= b[q]) {
                 if (a[p] >= b[q - 1]) {
-                    // a[p]是左半边最大的
+                    // 找到了p
                     re1 = re2 = a[p];
                     break;
                 } else {
                     // a[p]<b[q-1]
                     // p往右走 q往左走
-                    if (p == mr) {
-                        // p 在最右，并且b[q-1]是左半边最大的
+                    if (p == mr && a[p] <= b[q - 1]) {
                         re1 = re2 = b[q - 1];
-                        flag = 2;
                         break;
                     }
-                    if (q == nl) {
-                        // q在最左边，并且a[p]是左半边最大的
+                    if (q == nl && a[p] <= b[q]) {
                         re1 = re2 = a[p];
-                        flag = 1;
                         break;
                     }
                     temp = mr - (mr + p) / 2;
@@ -79,59 +73,52 @@ public class sc1_1 {
                     p += temp;
                 }
             } else {
-                // a[p] >= b[q]
-                if (q == nr) {
-                    // a[p] 已经比左半边最大的数大了，因此只能从a[p-1]和b[q]中取大的
-                    if (a[p - 1] >= b[q]) {
-                        // a[p-1]作为左边最大
-                        re1 = re2 = a[p - 1];
-                        flag = 3;
-                        break;
-                    } else {
-                        // b[q]作为左边最大
-                        re1 = re2 = b[q];
-                        flag = 4;
-                        break;
-                    }
-                }
-                if (b[q] >= a[p - 1]) {
-                    // b[q]作为左边最大
-                    re1 = re2 = b[q];
-                    flag = 4;
+                if (a[p - 1] >= b[q] && q == nr) {
+                    // a[p]>=b[q]&&a[p-1]>=b[q]
+                    re1 = re2 = a[p - 1];
                     break;
                 }
-                // b[q]<a[p-1]
-                // q往右走,p向左
-                temp = nr - (nr + q) / 2;
-                mr = q;
-                p -= temp;
-                nl = q;
-                q += temp;
+                // b[q]<a[p]
+                if (b[q] >= a[p - 1]) {
+                    // 找到了q
+                    re1 = re2 = b[q];
+                    break;
+                } else {
+                    // b[q]<a[p-1]
+                    // q往右走,p向左
+                    temp = nr - (nr + q) / 2;
+                    mr = q;
+                    p -= temp;
+                    nl = q;
+                    q += temp;
+                }
             }
         }
         if (m % 2 == 0 && n % 2 == 0) {
             re1 = a[p] > b[q - 1] ? a[p] : b[q - 1];
             re2 = a[p + 1] < b[q] ? a[p + 1] : b[q];
-        }
-        if (m % 2 != 0 && n % 2 != 0) {
-            if (flag == 1 ) {
-                // a[p]是左半边最大的
-                re2 = a[p + 1] < b[q] ? a[p + 1] : b[q];
-            } else if (flag == 2) {
-                // p 在最右，并且b[q-1]是左半边最大的
-                re2 = a[p] < b[q] ? a[p] : b[q];
-            } else if (flag == 3) {
-                // a[p-1]作为左半边最大，b[q]落在左半边
-                // 那么右半边最小一定是a[p]
-                re2 = a[p];
-            } else if (flag == 4) {
-                // b[q]作为左边最大,那么右半边最小为a[p]和b[q+1]中小的那个
+        } else if (m % 2 != 0 && n % 2 != 0) {
+            re2 = a[p + 1] < b[q] ? a[p + 1] : b[q];
+            if (q == nl)//
+            {
+
+                if (q != nr) {
+
+                    re1 = a[p - 1] > b[q] ? a[p - 1] : b[q];
+                    re2 = a[p] < b[q + 1] ? a[q] : b[q + 1];
+                } else {
+                    re1 = a[p - 1] > b[q] ? a[p - 1] : b[q];
+                    re2 = a[p] > b[q] ? a[p] : b[q];
+                }
+            } else if (q + 1 == nr)//
+            {
                 re2 = a[p] < b[q + 1] ? a[p] : b[q + 1];
             }
         }
         re1 = (re1 + re2) / 2;
         System.out.println(re1);
         sc.close();
+        // test change
     }
 }
 /*
