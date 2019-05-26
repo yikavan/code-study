e453e1e991fad241b848ff3ba4175ae6e8aac564
Yika <freedarknessdeep@gmail.com>
2019-05-26 21:41:32 +0800

    rename varible of sc1_1_cpoy

--- yika/src/main/java/c1/sc1_1_cpoy.java
+++ yika/src/main/java/c1/sc1_1_cpoy.java
@@ -16,32 +16,32 @@ class Solution {
         }
         int l = 0, r = m, mid = (m + n + 1) / 2;
         while (l <= r) {
-            int i = (l + r) / 2;
-            int j = mid - i;
-            if (i < r && B[j - 1] > A[i]) {
-                l = l + 1; // i is too small
-            } else if (i > l && A[i - 1] > B[j]) {
-                r = r - 1; // i is too big
-            } else { // i is perfect
+            int mida = (l + r) / 2;
+            int j = mid - mida;
+            if (mida < r && B[j - 1] > A[mida]) {
+                l = l + 1; // mida is too small
+            } else if (mida > l && A[mida - 1] > B[j]) {
+                r = r - 1; // mida is too big
+            } else { // mida is perfect
                 int maxLeft = 0;
-                if (i == 0) {
+                if (mida == 0) {
                     maxLeft = B[j - 1];
                 } else if (j == 0) {
-                    maxLeft = A[i - 1];
+                    maxLeft = A[mida - 1];
                 } else {
-                    maxLeft = Math.max(A[i - 1], B[j - 1]);
+                    maxLeft = Math.max(A[mida - 1], B[j - 1]);
                 }
                 if ((m + n) % 2 == 1) {
                     return maxLeft;
                 }
 
                 int minRight = 0;
-                if (i == m) {
+                if (mida == m) {
                     minRight = B[j];
                 } else if (j == n) {
-                    minRight = A[i];
+                    minRight = A[mida];
                 } else {
-                    minRight = Math.min(B[j], A[i]);
+                    minRight = Math.min(B[j], A[mida]);
                 }
 
                 return (maxLeft + minRight) / 2.0;
@@ -53,24 +53,26 @@ class Solution {
 
 public class sc1_1_cpoy {
     public static void main(String[] args) {
-            int[] a = new int[5], b = new int[3];
-            int m,n;
-            int i;
-            Scanner sc = new Scanner(System.in);
-            Solution ss = new Solution();
-            System.out.println("输入m,n:");
-            m = sc.nextInt();
-            n = sc.nextInt();
-            System.out.println("输入a[m]:");
-            for (i = 0; i < m; i++) {
-                a[i] = sc.nextInt();
-            }
-            System.out.println("输入b[n]:");
-            for (i = 0; i < n; i++) {
-                b[i] = sc.nextInt();
-            }
-            System.out.println(ss.findMedianSortedArrays(a,b));
-            sc.close();
+        int[] a, b;
+        int m, n;
+        int i;
+        Scanner sc = new Scanner(System.in);
+        Solution ss = new Solution();
+        System.out.println("输入m,n:");
+        m = sc.nextInt();
+        n = sc.nextInt();
+        a = new int[m];
+        b = new int[n];
+        System.out.println("输入a[m]:");
+        for (i = 0; i < m; i++) {
+            a[i] = sc.nextInt();
+        }
+        System.out.println("输入b[n]:");
+        for (i = 0; i < n; i++) {
+            b[i] = sc.nextInt();
+        }
+        System.out.println(ss.findMedianSortedArrays(a, b));
+        sc.close();
     }
 
 }
